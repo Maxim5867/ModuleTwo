f2eb181600f4e1f434e4c686a658e64a31eb77e7
Ваше Имя <you@example.com>
2022-10-22 16:02:00 +0300

    Работа на уроке

--- kubik.py
+++ kubik.py
@@ -64,28 +64,86 @@ def KubikiPlay():
 #*******************#
 #Функция с правилами#
 #*******************#
-def PravilaPlay():
-    print('Хотите ли вы прочитать правила? Да или нет?')
+def vopros(TextVoprosa):
     while True:
-        otvet = input().lower()
-        if (otvet == 'да') or (otvet == 'д') or (otvet == 'y') or (otvet == 'yes'):
-            print('''Представляю вам правила игры кубикию.
-            Игрок и компьтер поочередно пытаються набрать 21 очко, бросая два кубика.
+        print(TextVoprosa)
+        otvet = input()
+        otvet = otvet.lower()
+        if (otvet == 'да' ) or (otvet == 'д') or (otvet == 'yes') or (otvet == 'y'):
+            return True
+        elif (otvet == 'нет' ) or (otvet == 'н') or (otvet == 'no') or (otvet == 'n'):
+            return False
+        else:
+            print('Я вас не понял! Введте ответ еще раз')
+
+def pravilaGame():
+    print('''Представляю вам правила игры кубикию. Игрок и компьтер поочередно пытаються набрать 21 очко,
+             бросая два кубика.
             Человек начинаете первый, вы можете бросать кубики до тех пора не наберете 21 очко.
             Но учтите, если вы наберете больше 21 очка, сразу проиграете.
             Когда вы набрали определенное число,которым вы удовлетворены, можете передать ход компьютеру.
             Он будет кидать кубики до тех пор, пока не наберет число больше вашего.
             Если наберет больше 21, он програет, а вы выиграете.
             После вы сможете продолжить играть, если захотите''')
-            print('Нажмите Enter, чтобы продолжить')
-        else:
-            break
-#**************#
-#Функция броска#
-#**************#
-def BrosKuba():
-    for i in range(0,7):
-        TheKubiki = KubikiPlay
+    print()
+    s = input('Для продолжения нажмите Enter')
+
+def WhoFirst():
+    if random.randint(0,1) == 0:
+        return 'Компьютер'
+    else:
+        return 'Человек'
+
+def kubiki(k1,k2):
+    sz = []
+    sz.append(k1)
+    sz.append(k2)
+    return sz
+
+
+
+
+
+def display(gamer,image,k1,k2,sG,sK):
+    print(gamer)
+    print()
+    print(image(k1))
+    print(image(k2))
+    print()
+    print('У игрока - '+str(sG)+'. У компьютера - '+str(sK)+'.')
+
+
+#***********************#
+#Основное тело программы#
+#***********************#
+
+if vopros('Хотите прочитать правила? (да или нет)'):
+    help()
+
+ktoBrosaet = 'Человек'
+kub = kubiki()
+summaK = 0
+summaG = 0
+game = True
+gamer = True
+komputer = True
+
+
+while game:
+    while gamer:
+        kub1,kub2 = brosok()
+        summaG = summaG + kub1 + kub2
+        display(ktoBrosaet,kub,kub1,kub2,summaG,summaK)
+        if summaG > 21:
+            print('Вы проиграли')
+            game = False
+            gamer = False
+        if game and not (input('(Б)росаем еще или передаем ход?').upper() == 'Б'):
+            gamer = False
+
+    print('Передаем ход')
+
+            
 
 
 

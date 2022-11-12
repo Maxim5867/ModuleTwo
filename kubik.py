653f748c1952e1cfcc3be6fcc67f5f78a9558cb6
Ваше Имя <you@example.com>
2022-11-12 15:47:46 +0300

    Работа на уроке

--- kubik.py
+++ kubik.py
@@ -64,6 +64,25 @@ def KubikiPlay():
 #*******************#
 #Функция с правилами#
 #*******************#
+def brosok():
+    sz = []
+    k1 = random.randint(1,6)
+    k2 = random.randint(1,6)
+    sz.append(k1)
+    sz.append(k2)
+    return sz
+def playAgain():
+    print('Хотите ли вы сыграть ещё? Да или нет')
+    while True:
+        otvet = input(). lower()
+        if(otvet == 'да') or (otvet == 'д') or (otvet == 'yes') or (otvet == 'y'):
+            return True
+        elif(otvet == 'нет') or (otvet == 'н') or (otvet == 'no') or (otvet == 'n'):
+            return False
+        else:
+            print('''Я вас не понял, напишите пожалуйста да или нет''')
+
+
 def vopros(TextVoprosa):
     while True:
         print(TextVoprosa)
@@ -87,6 +106,13 @@ def pravilaGame():
             После вы сможете продолжить играть, если захотите''')
     print()
     s = input('Для продолжения нажмите Enter')
+def display(gamer,kubiki,kub1,kub2,sG,sK):
+    print(gamer)
+    print()
+    print(kubiki(kub1))
+    print(kubiki(kub2))
+    print()
+    print('У игрока - '+str(sG)+'. У компьютера - '+str(sK)+'.')
 
 def WhoFirst():
     if random.randint(0,1) == 0:
@@ -94,13 +120,6 @@ def WhoFirst():
     else:
         return 'Человек'
 
-def kubiki():
-    sz = []
-    k1 = random.randint(1,6)
-    k2 = random.randint(1,6)
-    sz.append(k1)
-    sz.append(k2)
-    return sz
 def hodP():
     print('Бросаем (Б) или передаем ход?')
     while True:
@@ -117,24 +136,7 @@ def proverkaBals(summaK,summaG):
         return False
     else:
         return True    
-def display(gamer,image,k1,k2,sG,sK):
-    print(gamer)
-    print()
-    print(image(k1))
-    print(image(k2))
-    print()
-    print('У игрока - '+str(sG)+'. У компьютера - '+str(sK)+'.')
 
-def playAgain():
-    print('Хотите ли вы сыграть ещё? Да или нет')
-    while True:
-        otvet = input(). lower()
-        if(otvet == 'да') or (otvet == 'д') or (otvet == 'yes') or (otvet == 'y'):
-            return True
-        elif(otvet == 'нет') or (otvet == 'н') or (otvet == 'no') or (otvet == 'n'):
-            return False
-        else:
-            print('''Я вас не понял, напишите пожалуйста да или нет''')
 
 
 #***********************#
@@ -145,7 +147,7 @@ if vopros('Хотите прочитать правила? (да или нет)'
     pravilaGame()
 
 ktoBrosaet = 'Человек'
-kub = kubiki()
+kub = brosok()
 summaK = 0
 summaG = 0
 game = True
@@ -156,9 +158,9 @@ pris = True
 
 while game:
     while gamer:
-        kub1,kub2 = kubiki()
+        kub1,kub2 = brosok()
         summaG = summaG + kub1 + kub2
-        display(ktoBrosaet,kub,kub1,kub2,summaG,summaK)
+        display(kub,kub1,kub2,summaG,summaK)
         if summaG > 21:
             print('Вы проиграли')
             game = False
@@ -170,9 +172,9 @@ while game:
 
     ktoBrosaet = 'Компьютер'
     while komputer:
-        kub1,kub2 = kubiki()
+        kub1,kub2 = brosok()
         summaK = summaK + kub1 + kub2
-        display(ktoBrosaet,kub,kub1,kub2,summaG,summaK)
+        display(kub,kub1,kub2,summaG,summaK)
         s = input('Для продолжения нажмите Enter')
         komputer = proverkaBals(summaG,summaK)
     
--- poisk.py
+++ poisk.py
@@ -12,7 +12,45 @@ def getPlayPole():
             else:
                 board[x].append('`')
     return board
+def displayBoard(board):
+    str0ne = '   '
+    for i in range(1,6):
+        str0ne += ' '*9 +str(i)
+    
+    print(str0ne)
+    print('   '+('0123456789'*6))
+    print()
+    
+    for stroka in range(15):
+        if stroka<10:
+            strPol = ' '
+        else:
+            strPol = ''
+
+        strBoard = ''
+        for stolbec in range(60):
+            strBoard += board[stolbec][stroka]
+        
+        print('%s%s %s %s' % (strPol,stroka, strBoard,stroka))
+
+    print()
+    print('   '+('0123456789'*6))
+    print(str0ne)
+
+
+def getRandomChests(kolChests):
+    chasts = []
+    while len(chasts)<=kolChests:
+        newChasts = [random.randint(0,59),random.randint(0,14)]
+        if newChasts not in chasts:
+            chasts.append(newChasts)
+    return chasts
+
+
+
+
 #***********************#
 #ОСНОВНОЕ ТЕЛО ПРОГРАММЫ#
 #***********************#
-print(getPlayPole())   
+theBoard = getPlayPole()
+displayBoard(theBoard)

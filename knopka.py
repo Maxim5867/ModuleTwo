14db1a85f14cc1b1d702a6b38ef4c6ad86017318
Ваше Имя <you@example.com>
2023-01-28 15:58:49 +0300

    Работа на уроке

--- knopka.py
+++ knopka.py
@@ -2,31 +2,41 @@ from tkinter import *
 from tkinter import ttk
 
 click = 0
-
-def x():
-    print(" ")
-def click_button():
-    global click
-    click +=1
-    btn["text"] = f"Нажатие {click}"
-    if click==1:
-        btn["text"] = f"1"
-    elif click==2:
-        btn["text"] = f"2"
-    elif click==3:
-        btn["text"] = f"3"
-    elif click==4:
-        btn["text"] = f"4"
-    else:
-        btn["text"] = f"Всё"
-        btn["state"] = [DISABLED]
-
 window = Tk()
 window.title('Кнопка')
 window.geometry('800x600')
+canvas = Canvas(window,width=800,height=600,background='green')
+def click_button():
+    global click
+    click +=1
+    canvas["background"] = 'black'
+    window.update_idletasks()
+    btn["text"] = f"Нажатие {click}"
+    if click==1:
+        btn["text"] = f"1"
+        canvas["background"] = 'red'
+    elif click==2:
+        btn["text"] = f"2"
+        canvas["background"] = 'yellow'
+    elif click==3:
+        btn["text"] = f"3"
+        canvas["background"] = 'blue'
+    elif click==4:
+        btn["text"] = f"4"
+        canvas["background"] = 'purple'
+    else:
+        btn["text"] = f"Всё"
+        canvas["background"] = 'white'
+        btn["state"] = [DISABLED]
+    
 
-btn = ttk.Button(text="Нажми меня", command=click_button)
-btn.pack(expand=True)
+
+
+
+btn = ttk.Button(window,text="Нажми меня", command=click_button)
+btn.pack()
+canvas.pack()
+
 
 window.mainloop()
 

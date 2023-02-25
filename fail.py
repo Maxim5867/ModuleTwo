e8cee87cebfb03becc1a20ee2c4569293f5b09b7
Ваше Имя <you@example.com>
2023-02-25 15:57:34 +0300

    Работа на уроке

--- fail.py
+++ fail.py
@@ -20,12 +20,13 @@ frame = Frame()
 frame.pack()
 
 fr1 = Frame()
-fr1.pack()
+
 
 ent = Entry(fr1,width=50)
-b = Button(fr1,text='Открыть',width=5,height=2,command=open_file)
-b1 = Button(fr1,text='Сохранить',width=5,height=2, command=save_file)
+b = Button(fr1,text='Открыть',width=6,height=2,command=open_file)
+b1 = Button(fr1,text='Сохранить',width=6,height=2, command=save_file)
 
+fr1.pack()
 ent.pack()
 b.pack(side=TOP)
 b1.pack(side=TOP)
@@ -33,7 +34,7 @@ b1.pack(side=TOP)
 fr2 = Frame()
 fr2.pack()
 
-text = Text(width=100,height=50)
+text = Text(fr2,width=100,height=50)
 text.pack()
 
 #fflf

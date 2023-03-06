85d10435994aa966c386767f5ddb84dafb5e97dc
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-03-06 22:29:06 +0300

    Update listbox2.py

--- listbox2.py
+++ listbox2.py
@@ -1,22 +1,58 @@
-from tkinter import *
+from tkinter import  *
 
 root = Tk()
 
-def zamena():
-    box = shop
-    box
+fr_1 = Frame()
+
+def sellect1():
+    select1 = box.curselection()
+
+    for i in select1:
+        box1.insert(END, box.get(i))
+
+    for i in reversed(select1):
+        box.delete(i)
 
 
-shop = ['яблоко морковь груша банан киви помидор огурец']
+def sellect2():
+    select2 = box1.curselection()
 
-box = Listbox(selectmode=EXTENDED, command=zamena)
-box1 = Listbox(selectmode=EXTENDED)
-b1 = Button(text = '>>>')
-b2 = Button(text = '<<<')
+    for i in select2:
+        box.insert(END, box1.get(i))
 
-box.pack()
-b1.pack()
-b2.pack()
-box1.pack()
+    for i in reversed(select2):
+        box1.delete(i)
+
+
+    
+
+shop = ['огурец', 'помидор', 'груша', 'банан', 'киви', 'морковь' , 'яблоко','свити','лук','чеснок', 'слива', 'малина','черника']
+
+box  = Listbox(selectmode=EXTENDED,height=10)
+box1 = Listbox(selectmode=EXTENDED,height=10)
+
+scroll = Scrollbar(command=box.yview)
+scroll.pack(side=LEFT,fill=Y)
+box.config(yscrollcommand=scroll.set)
+
+b1  = Button(fr_1,text  = '>>>')
+b2  = Button(fr_1,text  =  '<<<')
+
+scroll = Scrollbar(command=box1.yview)
+scroll.pack(side=RIGHT,fill=Y)
+box1.config(yscrollcommand=scroll.set)
+
+for i in shop:
+    box.insert(0,i)
+
+b1.bind('<Button-1>',lambda event:sellect1())
+b2.bind('<Button-1>',lambda event:sellect2())
+
+box.pack(side=LEFT)
+box1.pack(side=RIGHT)
+fr_1.pack(side=LEFT)
+b1.pack(fill=X)
+b2.pack(fill=X)
+
 
 root.mainloop()

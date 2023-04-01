3d204a58b26d46908956a9f4f99e8ba85c4e0b3f
Maxim5867 <102895647+Maxim5867@users.noreply.github.com>
2023-04-01 11:12:15 +0300

    Update 012.py

--- Otchet/012.py
+++ Otchet/012.py
@@ -4,15 +4,27 @@ root = Tk()
 
 def oval_func(event):
     c.delete(oval)
-    c.create_text(80, 50, text='Овал')
+    c.create_text(80, 50, text='Овал',tags='oval1')
+
+def oval_func1(event):
+    c.delete('oval1')
+    c.create_oval(30,10,130,80, fill='orange')
 
 def rect_func(event):
     c.delete(tril)
-    c.create_text(230, 50, text='Прямогульник')
+    c.create_text(230, 50, text='Прямогульник',tags='rect')
+
+def rect_func1(event):
+    c.delete('rect')
+    c.create_rectangle(180,10,280,80,fill='lightgreen')
 
 def triangle(event):
     c.delete(trian)
-    c.create_text(380, 50, text='Треугольник')
+    c.create_text(380,50,text='Треугольник',tags='triang')
+
+def triangle1(event):
+    c.delete('triang')
+    c.create_polygon(330,80,380,10,430,80,fill='white',outline='black')
 
 c = Canvas(width=460, height=100, bg='grey')
 c.pack()
@@ -24,5 +36,8 @@ trian = c.create_polygon(330,80,380,10,430,80,fill='white',outline='black')
 c.tag_bind(oval,'<Button-1>', oval_func)
 c.tag_bind(tril,  '<Button-1>', rect_func)
 c.tag_bind(trian, '<Button-1>', triangle)
+c.tag_bind('oval1','<Button-1>', oval_func1)
+c.tag_bind('rect',  '<Button-1>', rect_func1)
+c.tag_bind('triang', '<Button-1>', triangle1)
 
 root.mainloop()
